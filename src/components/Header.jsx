b9c8941b00494552b820b83b3f3ef294f2add4af
omkarg01 <omkargujja01@gmail.com>
2023-12-26 15:34:08 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import styled from "styled-components";
-import styles from "./NavBar.module.css";
+import styles from "./Navbar.module.css";
 
 const Header = () => {
   const location = useLocation();
